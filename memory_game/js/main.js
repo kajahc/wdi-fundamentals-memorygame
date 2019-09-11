e036cc73f17aa6c6cfc34259ad2236111f2df236
Kajah <kajahc@outlook.com>
2019-09-11 00:24:34 -0400

    recreated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,18 @@
+console.log("User flipped queen");
+console.log("User flipped king"); 
 const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = ["cardOne", "cardTwo"];
-console.log ("User flipped queen")
-var cardOne = cards[0]
-var cardTwo = cards[2]
-console.log ("User flipped king")
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-cardsInPlay.length;
-if  (cardsInPlay.length === 2) {
-}	else if (cardsInPlay[0] === cardsInPlay[1]){alert("You found a match!");} 
-	else {alert("Sorry, try again.");}
+const cardsInPlay = [];	
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+	console.log("User flipped " + cards[0]);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo)
+	console.log ("User flipped " + cards[3]);
+{if (cardsInPlay.length === 2); 
+else if (cards[0] === cards[1] || cards[2] === cards[3]) 
+	 {alert("You found a match!");} 
+else (cards[0] === cards[2] || cards[1] === cards[3]) 
+	{alert("Sorry, try again.");}
+}	
+ 
+
