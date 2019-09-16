eabd420b7b5a14440160bf48859cfa7d514460f0
Kajah <kajahc@outlook.com>
2019-09-16 12:10:12 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,16 @@
-console.log("User flipped queen");
-console.log("User flipped king"); 
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];	
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
 var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-	console.log("User flipped " + cards[0]);
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo)
-	console.log ("User flipped " + cards[3]);
-{if (cardsInPlay.length === 2); 
-else if (cards[0] === cards[1] || cards[2] === cards[3]) 
-	 {alert("You found a match!");} 
-else (cards[0] === cards[2] || cards[1] === cards[3]) 
-	{alert("Sorry, try again.");}
-}	
- 
+cardsInPlay.push(cards[0]);
+console.log("User flipped " + cards[0]);
+cardTwo = cards[2];
+cardsInPlay.push(cards[2]);
+console.log("User flipped " + cards[2]);
 
+if (cardsInPlay === 2) {
+	//what code goes here? What do we want the function to do?
+} else if (cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match!")
+  }else{ 
+  	alert("Sorry, try again.")
+  } 
